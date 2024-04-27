733e162e03f2d10c168763939b519e4291d56d81
Amele06 <kelvina0306@gmail.com>
2024-04-27 05:56:33 -0700

    package install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-#!/usr/bin/pup
-# Install flask (2.1.0)
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# 1-install_a_package.pp
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
+
+# Notify success
+notify { 'Flask installed successfully':
+  require => Exec['install_flask'],
 }
